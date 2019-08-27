42f643aac774a6c420a06f7df13636a236bcd4b2
SmiledElf <jason.kwe@gmail.com>
2019-08-27 20:51:01 +1000

    Barebones functionality complete with no known bugs.

--- screenshots-renamer.py
+++ screenshots-renamer.py
@@ -4,55 +4,42 @@ import time
 
 
 def main():
+    """
+    This program loops through the folder it's placed in and looks for .png files that start with "Screenshot", then
+    renames them according to day of creation without duplicates.
+    """
     # Get current directory, then loop through its files to find which ones are screenshots
-    current_folder = os.curdir
+    current_directory = os.curdir
 
-    for file in os.listdir(current_folder):
+    for file in os.listdir(current_directory):
         if file.startswith("Screenshot") and file.lower().endswith(".png"):
             # If a screenshot is found, get its creation metadata
             filepath = os.path.join(os.curdir, file)
             filestatobject = os.stat(filepath)
 
-            # Create list containing information about file creation time
+            # Get file creation time
             creation_time = time.ctime(filestatobject[stat.ST_CTIME]).split()  # e.g [Mon, Jul, 8, 01:16:44, 2019]
-
-            # Put into readable format, then rename file accordingly
-            # e.g 08Jul2019_1
             creation_day = "{:02d}".format(int(creation_time[2]))
             creation_month = str(creation_time[1])
             creation_year = str(creation_time[-1])
+            duplicate_index = 1
 
-            proposed_name = creation_day + creation_month + creation_year + '.png'  # Readable format
-            correct_name = duplicate_resolution(proposed_name, 0)   # Readable format AND no duplicates
+            # Propose name with no duplicates
+            # e.g 26Aug2019_1
+            proposed_filename = creation_day + creation_month + creation_year + '_' + str(duplicate_index) + '.png'
+            duplicate_exist = os.path.isfile(proposed_filename)
 
-            os.rename(filepath, correct_name)
+            while duplicate_exist:
+                print(proposed_filename, duplicate_exist)
+                duplicate_index += 1
+                proposed_filename = creation_day + creation_month + creation_year + '_' + str(duplicate_index) + '.png'
 
+                duplicate_exist = os.path.isfile(proposed_filename)
 
-def duplicate_resolution(filename, increment):
-    """
-    This function determines whether a file already exists with the proposed name change.
-    If one does, the function determines a unique name for it.
-
-    :param filename: current proposed filename
-    :param increment: current index of duplicate (i.e how many duplicates there exists)
-    :return: new_filename: filename to be changed
-    """
-
-    duplicate_exists = os.path.isfile(os.curdir + filename)
-    print(duplicate_exists)
-
-    if increment == 0 and not duplicate_exists:
-        print("No dupes exists, returning now")
-        print(filename, increment)
-        return filename
-    else:
-        print("Dupe found!")
-        next_increment = increment + 1
-        new_filename = filename[:-4] + "_" + str(next_increment) + filename[-4:]
-
-        print(new_filename, next_increment)
-
-        duplicate_resolution(new_filename, next_increment)
+            try:
+                os.rename(filepath, proposed_filename)
+            except FileExistsError:
+                print("ERROR: Cannot rename", filepath, "to", proposed_filename + "! File already exists.")
 
 
 if __name__ == "__main__":
