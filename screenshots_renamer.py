024adaa1114b39de4345dcd87f85b9f632d79db1
SmiledElf <j.jason.k@hotmail.com>
2024-06-18 22:07:32 +1000

    change debug flag to False

--- screenshots_renamer.py
+++ screenshots_renamer.py
@@ -14,7 +14,7 @@ def main():
     """
     
     # we dedicate this block for debugging :)
-    debug = True
+    debug = False
     if debug:
         test_folder = "test_screenshots_duplicates" # <--- change here
         script_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), test_folder)
