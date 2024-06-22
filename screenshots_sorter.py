7ddb18cb1c62b95af1d2763676318290d5cc98f1
SmiledElf <j.jason.k@hotmail.com>
2024-06-22 15:19:48 +1000

    use loguru for logging

--- screenshots_sorter.py
+++ screenshots_sorter.py
@@ -1,8 +1,10 @@
 import os
 import steamfront
-import logging
+from loguru import logger
+from sys import stdout
 
-# TODO: this class? do i even need tbh?
+
+
 class ScreenshotSorter():
 
     client = steamfront.Client()
@@ -10,36 +12,39 @@ class ScreenshotSorter():
 
 
     def __init__(self, root_dir:str) -> None:
+        """Instantiate screenshot sorter based on directory given.
         
-        # initialise instance variables
+        :param root_dir: directory to scan
+        """
+        
+        logger.debug("Instantiated ScreenshotSorter")
+
         self.root_dir = root_dir
         self.cache = dict()  # { appid : game object }
-        
-        # set up logging
-        logging.basicConfig(filename='sorting.log', encoding='utf-8', level=logging.DEBUG)
-
+    
 
     def sort_screenshots(self, directory:str=None) -> None:
 
-        logging.info("Steam interface loaded, begin scanning directory")
+        logger.info("Beginning screenshots sorting")
+
         if directory is None:
             directory = self.root_dir  # this lets us call sort_screenshots() without specifying path if it has been instantiated
 
         for file in os.listdir(directory):
             if file.endswith(ScreenshotSorter.accepted_extensions):
-                logging.info(f"Detected screenshot {file}")
+                logger.debug(f"Detected screenshot {file}")
 
                 # get the game of the screenshot
                 screenshot_appid = file.split("_")[0]  # appid
                 game_name = ""
                 if screenshot_appid in self.cache:
                     game_name = self.cache[screenshot_appid]
-                    logging.info(f"Game ({game_name}) previously detected, no need to call API again to retrieve name")
+                    logger.debug(f"Game ({game_name}) previously detected, no need to call API again to retrieve name")
                 else:
                     # retrieve game object, get the game name, and store to dict
                     game_name = client.getApp(appid=screenshot_appid).name
                     self.cache[screenshot_appid] = game_name
-                    logging.info(f"New game detected, API called to get the name of the game ({game_name})")
+                    logger.debug(f"New game detected, API called to get the name of the game ({game_name})")
 
                 
                 # validate game name for Windows folder name (Windows)
@@ -47,41 +52,40 @@ class ScreenshotSorter():
                 folder_name = game_name
                 for v, k in invalid_mapping:
                     folder_name = folder_name.replace(v, k)
-                logging.info(f"Finished validating game name for Windows folder names")
+                logger.debug(f"Finished validating game name for Windows folder names")
 
 
                 # create game folder if it doesn't exist
                 if not os.path.isdir(folder_name):
-                    logging.error(f"Folder {folder_name} could not be found")
+                    logger.error(f"Folder {folder_name} could not be found")
                     try:
                         os.mkdir(folder_name)
-                        logging.info(f"Created folder {folder_name}")
+                        logger.debug(f"Created folder {folder_name}")
                     except Exception as e:
-                        logging.error(f"Failed to create folder {folder_name}. Exception: {e}")
+                        logger.error(f"Failed to create folder {folder_name}. Exception: {e}")
                 
                 # move screenshot to its game folder
                 file_moved = f"./{folder_name}/{file}"
                 try:
                     os.rename(file, file_moved)
-                    logging.info(f"Moved file from {file} to {file_moved}")
+                    logger.debug(f"Moved file from {file} to {file_moved}")
                 except Exception as e:
-                    logging.error(f"Failed to moved file from {file} to {file_moved}. Exception: {e}")
+                    logger.error(f"Failed to moved file from {file} to {file_moved}. Exception: {e}")
 
+        logger.info("Finished screenshots sorting")
 
 if __name__ == "__main__":
 
-    logging.basicConfig(filename='sorting.log', encoding='utf-8', level=logging.DEBUG)
-    logging.info("Logging initialised, starting script now.")
-
-    script_path = os.path.realpath(__file__)
+    logger.info("Main function starting.")
+    script_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(script_path)
-    logging.debug(f"Set current working directory to {script_path}")
+    logger.debug(f"Set current working directory to {script_path}")
 
     client = steamfront.Client()
-    logging.debug("Steam API loaded, beginning renaming function.")
+    logger.debug("Initialised steamfront to query Steam API")
+
     sorter = ScreenshotSorter(script_path)
     sorter.sort_screenshots()
-    logging.info("Main module is finished, closing script")
-    print("Script is done!")
-         
-
+    
+    logger.info("Main function finished, closing script")
+    logger.add("screenshots_sorter.log")
